55fe33e610c6f25e5590a6d4d658eee923a78561
by-westik <nastya.derevyanko.2020@mail.ru>
2023-05-06 16:09:24 +0300

    Initial commit

---
+++ app/src/main/java/com/westik/file/me/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.westik.file.me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
